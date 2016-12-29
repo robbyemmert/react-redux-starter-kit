7db00f91e877c924665be005d18b0b40f83fc8df
Robby Emmert <robby.emmert@cqlcorp.com>
2016-12-29 11:59:03 -0500

    wrapping dev code in process env check

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,10 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducers';
 
 let store = applyMiddleware(thunkMiddleware)(createStore)(reducers);
-window.s = store;
+
+// Make the store available on the window object for debugging purposes
+if (process.env.NODE_ENV === 'development') {
+    window.store = store;
+}
+
 export default store;
